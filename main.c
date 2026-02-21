51114518b67bd1d9d55c5a9cfcd01c87b34272e2
David Ayala <david.ayala@outlook.com>
2026-02-21 00:07:14 -0600

    Definir firmas de funciones futuras

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #define N 4
 
-
+void InicializarMatriz(float matriz[N][N]);
+void LeerMatriz(float matriz[N][N]);
+void CalcularMatrizTriangularSuperior(float matriz[N][N]);
+void PromoverFilaConMaximoAlPivote(float matriz[N][N], int filaPivote, int columnaPivote);
+void HacerCerosLosElementosBajoElPivote(float matriz[N][N], int filaPivote, int columnaPivote);
+void IntercambiarFilas(float matriz[N][N], int fila1, int fila2);
+void MostrarMatriz(float matriz[N][N]);
 
 int main() {
     // variables/declaraciones
     float matrizDeCoeficientes[N][N];
 
     // entradas
-
 
     // proceso
 
