aa8e1156c9117e86884dc495c0ebb04fe1284ed7
David Ayala <david.ayala@outlook.com>
2026-02-21 00:11:49 -0600

    Definicion de main

--- main.c
+++ main.c
@@ -3,18 +3,26 @@
 
 void InicializarMatriz(float matriz[N][N]);
 void LeerMatriz(float matriz[N][N]);
-void EliminacionGaussianaConPivotajeParcial(float matriz[N][N]);
+void CalcularMatrizTriangularSuperior(float matriz[N][N]);
 void PromoverFilaConMaximoAlPivote(float matriz[N][N], int filaPivote, int columnaPivote);
 void HacerCerosLosElementosBajoElPivote(float matriz[N][N], int filaPivote, int columnaPivote);
 void IntercambiarFilas(float matriz[N][N], int fila1, int fila2);
+void MostrarMatriz(float matriz[N][N]);
 
 int main() {
     // variables/declaraciones
     float matrizDeCoeficientes[N][N];
 
+    InicializarMatriz(matrizDeCoeficientes);
+
     // entradas
+    LeerMatriz(matrizDeCoeficientes);
 
     // proceso
+    CalcularMatrizTriangularSuperior(matrizDeCoeficientes);
 
     // salida
+    MostrarMatriz(matrizDeCoeficientes);
+
+    return 0;
 }
